c5f2f7786d84adcb721b228b08ebee622ea487cb
Daniel Zhang <dzhang35@student.ubc.ca>
2023-02-24 00:52:10 -0700

    Finished implementing methods for MoveList, fixed two of the tests for these methods.

--- src/main/model/MoveList.java
+++ src/main/model/MoveList.java
@@ -1,18 +1,24 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 
 // class for list of moves in computer and human-readable notation
 public class MoveList {
     // INVARIANT: moves.size() == humanList.size()
-    private ArrayList<Move> moves; // list of moves for computer
-    private ArrayList<String> humanList; // list of moves for human players to see
+    private final ArrayList<Move> moves; // list of moves for computer
+    private final ArrayList<String> notationList; // list of moves for human players to see
+
+    // list of chess columns for reference
+    private static final ArrayList<String> columns = new ArrayList<>(
+            Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h"));
 
     // MODIFIES: this
     // EFFECTS: creates an empty list of moves, and human algebraic translations
     public MoveList() {
-
+        moves = new ArrayList<>();
+        notationList = new ArrayList<>();
     }
 
     // EFFECTS: copies and returns a move list
@@ -21,8 +27,8 @@ public class MoveList {
     }
 
     // EFFECTS: copies and returns a human-readable list
-    public ArrayList<String> getHumanList() {
-        return new ArrayList<>(humanList);
+    public ArrayList<String> getNotationList() {
+        return new ArrayList<>(notationList);
     }
 
     // REQUIRES: move is a valid move, and start and end squares are correct; piece should be copies of piece on board
@@ -30,43 +36,70 @@ public class MoveList {
     // MODIFIES: this
     // EFFECTS: adds a non-capture move to both arrays of MoveList (this includes castling); one for humans and one for
     // the code, such that all information of the move is independent of the board state.
-    public void addMove(Piece piece, int start, int end, boolean inCheck) {
-
+    public void addMove(Move move) {
+        moves.add(move);
+        notationList.add(toNotation(move));
     }
 
-    // REQUIRES: move is a valid move, and start and end squares are correct, captured piece is being taken off board
-    // all pieces passed to method should be copies of pieces on board on original positions.
-    // MODIFIES: this
-    // EFFECTS: adds a capture move to both arrays of MoveList (this includes en-passant), this will save the piece
-    // object being captured inside ArrayList 'moves'
-    public void addMove(Piece piece, int start, int end, boolean inCheck, Piece captured) {
+    private String toNotation(Move move) {
+        if (move.getPiece().getName().equals("K")) {
+            if ((move.getStart() == 60 && move.getEnd() == 62) || (move.getStart() == 4 && move.getEnd() == 6)) {
+                return "O-O";
+            } else if ((move.getStart() == 60 && move.getEnd() == 58) || (move.getStart() == 4 && move.getEnd() == 2)) {
+                return "O-O-O";
+            }
+        }
+        String piece = move.getPiece().getName();
+        if (piece.equals("P")) {
+            piece = "";
+        }
+        String capture = "";
+        String check = "";
+        if (move.isCheck()) {
+            check = "+";
+        }
+        if (move.getCaptured() != null) {
+            if (piece.isEmpty()) {
+                piece = columns.get(move.getStart() % 8);
+            }
+            capture = "x";
+        }
+        return piece + capture + getCoordinate(move.getEnd()) + check;
+    }
 
+    private String getCoordinate(int square) {
+        return  columns.get(square % 8) + (8 - square / 8);
     }
 
     // REQUIRES: at least one move was made, and previous move is a check (otherwise it can't be checkmate)
     // MODIFIES: this
-    // EFFECTS: adds "#" symbol to end of previous move on humanList. If white won, calls addResult to make white the
+    // EFFECTS: adds "#" symbol to end of previous move of notation. If white won, calls addResult to make white the
     // victor, else calls addResult to make black the victor
     public void wasCheckmate() {
-
+        String move = notationList.get(notationList.size() - 1);
+        move = move.substring(0, move.length() - 1) + "#";  // this replaces the "+" with "#"
+        notationList.remove(notationList.size() - 1);
+        notationList.add(move);
     }
 
     // REQUIRES: score is either '1-0', '0-1', or '1/2-1/2'
     // MODIFIES: this
-    // EFFECTS: adds score to end of humanList to signify game end
+    // EFFECTS: adds score to end of notation list to signify game end
     public void addResult(String score) {
-
+        notationList.add(score);
     }
 
     // REQUIRES: at least one move was made
     // MODIFIES: this
     // EFFECTS: removes the latest move from both arrays
     public void undo() {
+        moves.remove(moves.size() - 1);
+        notationList.remove(notationList.size() - 1);
     }
 
     // REQUIRES: at least one move was made
     // EFFECTS: returns the last move in array
     public Move getPreviousMove() {
-        return null;
+        return moves.get(moves.size() - 1);
     }
 }
--- src/test/model/MoveListTest.java
+++ src/test/model/MoveListTest.java
@@ -15,15 +15,15 @@ public class MoveListTest {
     @Test
     public void testConstructor() {
         assertEquals(0, testList.getMoves().size());
-        assertEquals(0, testList.getHumanList().size());
+        assertEquals(0, testList.getNotationList().size());
     }
 
     @Test
     public void testAddMove() {
         Pawn testPiece = new Pawn("W", 52);
-        testList.addMove(testPiece, 52, 36, false);
-        assertEquals(1, testList.getHumanList().size());
-        assertEquals("e4", testList.getHumanList().get(0));
+        testList.addMove(new Move(testPiece, 52, 36, false));
+        assertEquals(1, testList.getNotationList().size());
+        assertEquals("e4", testList.getNotationList().get(0));
         Move firstMove = testList.getMoves().get(0);
         assertEquals(52, firstMove.getStart());
         assertEquals(36, firstMove.getEnd());
@@ -39,17 +39,17 @@ public class MoveListTest {
         // this is a special case where the square of capture and ending square are not the same
         Pawn testPiece = new Pawn("B", 36);  // note these pieces are only copies of pieces on board
         Pawn capturedPiece1 = new Pawn("W", 51);
-        testList.addMove(capturedPiece1, 51, 35, false);
+        testList.addMove(new Move(capturedPiece1, 51, 35, false));
         Pawn capturedPiece2 = new Pawn("W", 35);  // we must create a new piece
-        testList.addMove(testPiece, 36, 43, false, capturedPiece2);
-        assertEquals(2, testList.getHumanList().size());
-        assertEquals("exd3", testList.getHumanList().get(1));  // google 'en passant'
-        assertEquals("d4", testList.getHumanList().get(0));
+        testList.addMove(new Move(testPiece, 36, 43, false, capturedPiece2));
+        assertEquals(2, testList.getNotationList().size());
+        assertEquals("exd3", testList.getNotationList().get(1));  // google 'en passant'
+        assertEquals("d4", testList.getNotationList().get(0));
 
         Move previousMove = testList.getPreviousMove();
         assertEquals(36, previousMove.getStart());
         assertEquals(43, previousMove.getEnd());
-        assertEquals(43, previousMove.getPiece().getPosition());
+        assertEquals(36, previousMove.getPiece().getPosition());
         assertFalse(previousMove.isCheck());
         assertNotNull(previousMove.getCaptured());
         assertEquals(35, previousMove.getCaptured().getPosition());
@@ -57,32 +57,32 @@ public class MoveListTest {
 
     @Test
     public void testIsCheckmate() {
-        testList.addMove(new Queen("W", 45), 45, 13, true, new Pawn("B", 13));
-        assertEquals(1, testList.getHumanList().size());
-        assertEquals("Qxf7+", testList.getHumanList().get(0));
+        testList.addMove(new Move(new Queen("W", 45), 45, 13, true, new Pawn("B", 13)));
+        assertEquals(1, testList.getNotationList().size());
+        assertEquals("Qxf7+", testList.getNotationList().get(0));
         testList.wasCheckmate();
-        assertEquals(1, testList.getHumanList().size());
-        assertEquals("Qxf7#", testList.getHumanList().get(0));
+        assertEquals(1, testList.getNotationList().size());
+        assertEquals("Qxf7#", testList.getNotationList().get(0));
     }
 
     @Test
     public void testAddScore() {
-        testList.addMove(new King("W", 52), 60, 52, false);
+        testList.addMove(new Move(new King("W", 52), 60, 52, false));
         testList.addResult("1/2-1/2");
-        assertEquals(2, testList.getHumanList().size());
-        assertEquals("Ke7", testList.getHumanList().get(0));
-        assertEquals("1/2-1/2", testList.getHumanList().get(1));
+        assertEquals(2, testList.getNotationList().size());
+        assertEquals("Ke2", testList.getNotationList().get(0));
+        assertEquals("1/2-1/2", testList.getNotationList().get(1));
 
     }
 
     @Test
     public void testUndoMove() {
         Pawn testPiece = new Pawn("W", 52);
-        testList.addMove(testPiece, 52, 36, false);
-        assertEquals(1, testList.getHumanList().size());
+        testList.addMove(new Move(testPiece, 52, 36, false));
+        assertEquals(1, testList.getNotationList().size());
         testList.undo();
         assertEquals(0, testList.getMoves().size());
-        assertEquals(0, testList.getHumanList().size());
+        assertEquals(0, testList.getNotationList().size());
     }
     // while it may be fundamentally different to undo a capture vs non-capture on an actual game board, the process for
     // MoveList is pretty much the exact same, so there doesn't need to be another test for undo captures.
