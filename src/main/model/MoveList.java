4a3872797bfbb6b45ea2491d5bb3d3020ec29d61
Daniel Zhang <dzhang35@student.ubc.ca>
2023-02-25 14:55:20 -0700

    Added new method in MoveList class to get number of moves/size of list.

--- src/main/model/MoveList.java
+++ src/main/model/MoveList.java
@@ -4,12 +4,11 @@ import model.exceptions.NotValidSquareException;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-//todo tests and methods for 3 move repetition and 50 move rule
 
 // class for list of moves in computer and human-readable notation
-// also translates between board coordinates and algebraic notation
+// therefore can also translate between board coordinates and algebraic notation
 public class MoveList {
-    // INVARIANT: moves.size() == humanList.size()
+    // INVARIANT: both arrays have same .size()
     private final ArrayList<Move> moves; // list of moves for computer
     private final ArrayList<String> notationList; // list of moves for human players to see
 
@@ -29,19 +28,24 @@ public class MoveList {
         return new ArrayList<>(moves);
     }
 
+    // EFFECTS: returns the size of arrays
+    public int getSize() {
+        return notationList.size();
+    }
+
     // EFFECTS: copies and returns a human-readable list
     public ArrayList<String> getNotationList() {
         return new ArrayList<>(notationList);
     }
 
-    // REQUIRES: move is a valid move, and start and end squares are correct; piece should be copies of piece on board
-    // on its original square
+    // REQUIRES: move is instantiated with correct information, and start and end squares are correct; piece objects
+    // should be copies of the pieces on board on its original square, prior to moving
     // MODIFIES: this
-    // EFFECTS: adds a non-capture move to both arrays of MoveList (this includes castling); one for humans and one for
-    // the code, such that all information of the move is independent of the board state.
+    // EFFECTS: adds a move to both arrays of MoveList (this includes castling) in appropriate formats
     public void addMove(Move move) {
         moves.add(move);
         notationList.add(toNotation(move));
+        assert moves.size() == notationList.size();
     }
 
     private String toNotation(Move move) {
@@ -70,6 +74,7 @@ public class MoveList {
         return piece + capture + fromCoordinate(move.getEnd()) + check;
     }
 
+    // REQUIRES: 0 <= square < 64
     // EFFECTS: converts a coordinate into algebraic notation
     public static String fromCoordinate(int square) {
         return  files.get(square % 8) + (8 - square / 8);
@@ -118,6 +123,7 @@ public class MoveList {
     public void undo() {
         moves.remove(moves.size() - 1);
         notationList.remove(notationList.size() - 1);
+        assert moves.size() == notationList.size();
     }
 
     // REQUIRES: at least one move was made
--- src/test/model/MoveListTest.java
+++ src/test/model/MoveListTest.java
@@ -17,13 +17,14 @@ public class MoveListTest {
     public void testConstructor() {
         assertEquals(0, testList.getMoves().size());
         assertEquals(0, testList.getNotationList().size());
+        assertEquals(0, testList.getSize());
     }
 
     @Test
     public void testAddMove() {
         Pawn testPiece = new Pawn("W", 52);
         testList.addMove(new Move(testPiece, 52, 36, false));
-        assertEquals(1, testList.getNotationList().size());
+        assertEquals(1, testList.getSize());
         assertEquals("e4", testList.getNotationList().get(0));
         Move firstMove = testList.getMoves().get(0);
         assertEquals(52, firstMove.getStart());
@@ -53,7 +54,7 @@ public class MoveListTest {
         testList.addMove(new Move(capturedPiece1, 51, 35, false));
         Pawn capturedPiece2 = new Pawn("W", 35);  // we must create a new piece
         testList.addMove(new Move(testPiece, 36, 43, false, capturedPiece2));
-        assertEquals(2, testList.getNotationList().size());
+        assertEquals(2, testList.getSize());
         assertEquals("exd3", testList.getNotationList().get(1));  // google 'en passant'
         assertEquals("d4", testList.getNotationList().get(0));
 
@@ -69,7 +70,7 @@ public class MoveListTest {
     @Test
     public void testIsCheckmate() {
         testList.addMove(new Move(new Queen("W", 45), 45, 13, true, new Pawn("B", 13)));
-        assertEquals(1, testList.getNotationList().size());
+        assertEquals(1, testList.getSize());
         assertEquals("Qxf7+", testList.getNotationList().get(0));
         testList.wasCheckmate();
         assertEquals(1, testList.getNotationList().size());
@@ -80,7 +81,7 @@ public class MoveListTest {
     public void testAddScore() {
         testList.addMove(new Move(new King("W", 52), 60, 52, false));
         testList.addResult("1/2-1/2");
-        assertEquals(2, testList.getNotationList().size());
+        assertEquals(2, testList.getSize());
         assertEquals("Ke2", testList.getNotationList().get(0));
         assertEquals("1/2-1/2", testList.getNotationList().get(1));
 
@@ -90,10 +91,10 @@ public class MoveListTest {
     public void testUndoMove() {
         Pawn testPiece = new Pawn("W", 52);
         testList.addMove(new Move(testPiece, 52, 36, false));
-        assertEquals(1, testList.getNotationList().size());
+        assertEquals(1, testList.getSize());
         testList.undo();
         assertEquals(0, testList.getMoves().size());
-        assertEquals(0, testList.getNotationList().size());
+        assertEquals(0, testList.getSize());
     }
     // while it may be fundamentally different to undo a capture vs non-capture on an actual game board, the process for
     // MoveList is pretty much the exact same, so there doesn't need to be another test for undo captures.
@@ -119,17 +120,9 @@ public class MoveListTest {
 
     @Test
     public void testToCoordinateExceptions() {
-        assertThrows(NotValidSquareException.class, () -> {
-            MoveList.toCoordinate("b12");
-        });
-        assertThrows(NotValidSquareException.class, () -> {
-            MoveList.toCoordinate("6e");
-        });
-        assertThrows(NotValidSquareException.class, () -> {
-            MoveList.toCoordinate("i3");
-        });
-        assertThrows(NotValidSquareException.class, () -> {
-            MoveList.toCoordinate("d0");
-        });
+        assertThrows(NotValidSquareException.class, () -> MoveList.toCoordinate("b12"));
+        assertThrows(NotValidSquareException.class, () -> MoveList.toCoordinate("6e"));
+        assertThrows(NotValidSquareException.class, () -> MoveList.toCoordinate("i3"));
+        assertThrows(NotValidSquareException.class, () -> MoveList.toCoordinate("d0"));
     }
 }
